d0cdfb12b6ebb1b42e607b1ce201160155b16f37
Arthur <aamshukov@gmail.com>
2022-12-21 21:34:08 -0500

    Initial VS2022 project.

--- src/graphs.py
+++ src/graphs.py
@@ -1,1 +1,14 @@
+import sys
 
+
+def main(args):
+    try:
+        pass
+    except Exception as ex:
+        print(ex)
+    return 1
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    sys.exit(main(sys.argv[1:]))
