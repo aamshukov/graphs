4fe4864929d54be9f8cc08f52a221a824fcb3f2c
Arthur <aamshukov@gmail.com>
2022-12-23 22:24:08 -0500

    Unit tests

--- src/adt/graph.py
+++ src/adt/graph.py
@@ -4,16 +4,52 @@
 #
 """ Graph data type """
 from ..core.entity import Entity
+from vertex import Vertex
+from edge import Edge
 
 
 class Graph(Entity):
     """
     """
 
-    def __init__(self, id=0, version='1.0'):
+    def __init__(self, id=0, digraph=False, version='1.0'):
         """
         """
         super().__init__(id, version)
+        self._root = None  # optional, used in some digraph algorithms
+        self._digraph;  # directed or not
+        self._vertices = set()
+        self._edges = set()
+
+    @property
+    def root(self):
+        """
+        """
+        return self._root
+
+    @root.setter
+    def root(self, root):
+        """
+        """
+        self._root = root
+
+    @property
+    def digraph(self):
+        """
+        """
+        return self._digraph
+
+    @property
+    def vertices(self):
+        """
+        """
+        return self._vertices
+
+    @property
+    def edges(self):
+        """
+        """
+        return self._edges
 
     def validate(self):
         """
